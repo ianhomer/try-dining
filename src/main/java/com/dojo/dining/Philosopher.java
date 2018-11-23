60c3d834053f3a2375e104e164fe911b4cd8a73e
Adam Birse <abirse@worth.systems>
2018-11-23 09:52:46 +0000

    eating and thinking

--- src/main/java/com/dojo/dining/Philosopher.java
+++ src/main/java/com/dojo/dining/Philosopher.java
@@ -1,4 +1,12 @@
 package com.dojo.dining;
 
 public class Philosopher {
+
+    public void eating() {
+        System.out.println("Eating");
+    }
+
+    public void thinking() {
+        System.out.println("Thinking");
+    }
 }
