5b5ba90ace8aad768fdac0e510cd8b7d41e1b36a
Ian Homer <ian.homer@gmail.com>
2018-11-23 21:56:38 +0000

    Kotlin deadlock : better idioms

--- dining.kts
+++ dining.kts
@@ -10,17 +10,12 @@ class Philospher(val index: Int, val left: Any, val right: Any) {
             }
         }
     })
-    fun eat() {
-        diner.start()
-    }
-
-    fun isEating() : Boolean {
-        return diner.isAlive()
-    }
+    var eat = { diner.start() }
+    fun isEating() : Boolean { return diner.isAlive() }
 }
 
 val size = 4
-val forks = listOf(Object(), Object(), Object(), Object())
+val forks = (1..4).map { Object() }
 val philosphers = listOf(
             Philospher(1, forks[0], forks[1]),
             Philospher(2, forks[1], forks[2]),
@@ -34,7 +29,7 @@ println("... all eating")
 var eating = true
 while (eating) {
     Thread.sleep(1_000)
-    var count = philosphers.count({it -> it.isEating()})
+    var count = philosphers.count {it -> it.isEating()}
     eating = count != 0
     if (eating) println("... ${count} count still eating")
 }
